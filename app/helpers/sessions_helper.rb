f85b41580dc7c0c3d7824b676e98ff2bf1d787f3
Nguyen Ngoc Diep <ndiep2210@gmail.com>
2024-10-16 13:46:38 +0700

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,37 @@
 module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
+    # Guard against session replay attacks.
+    # See https://bit.ly/33UvK0w for more.
+    session[:session_token] = user.session_token
   end
 
-  # Returns the current logged-in user (if any).
+  # remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Returns the user corresponding to the remember token cookie.
   def current_user
-    if session[:user_id] # means user is logged in
-      @current_user ||= User.find_by(id: session[:user_id]) # memoization: check if @current_user is already set, if not, set it to the user found by id
+    if (user_id = session[:user_id]) # session[:user_id] is not nil
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
+    elsif (user_id = cookies.encrypted[:user_id]) # cookies[:user_id] is not nil
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
+  end
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -16,7 +40,20 @@ module SessionsHelper
   end
 
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil
+  end
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_10_074254) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_16_055046) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
+    t.string "string"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
